bac6661b8ad71bb750aa113537518f53eb4a0f23
jakubriegel <jakubriegel112@gmail.com>
2020-04-12 18:35:38 +0200

    active searches from repo

--- src/main/scala/eu/jrie/put/cs/pt/scrapper/search/SearchExecutor.scala
+++ src/main/scala/eu/jrie/put/cs/pt/scrapper/search/SearchExecutor.scala
@@ -7,18 +7,18 @@ import java.util.UUID.randomUUID
 
 import akka.NotUsed
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.stream.alpakka.slick.scaladsl.{Slick, SlickSession}
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.Source
+import akka.util.Timeout
 import com.redis.RedisClient
-import eu.jrie.put.cs.pt.scrapper.model.db.Tables.SearchesParamsTable.SearchParamRow
-import eu.jrie.put.cs.pt.scrapper.model.db.Tables.SearchesTable.SearchRow
 import eu.jrie.put.cs.pt.scrapper.redis.Message.TaskMessage
 import eu.jrie.put.cs.pt.scrapper.redis.Publisher
 import eu.jrie.put.cs.pt.scrapper.redis.Publisher.{EndPublish, Publish}
+import eu.jrie.put.cs.pt.scrapper.search.SearchRepository.{EndSearchRepo, GetActiveSearches, SearchRepoMsg, SearchesAnswer}
 
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 
 object SearchExecutor {
@@ -28,43 +28,63 @@ object SearchExecutor {
   private implicit val session: SlickSession = SlickSession.forConfig("slick-mysql")
   import session.profile.api._
 
-  def apply(redis: RedisClient): Behavior[StartSearch] = Behaviors.receive { (context, _) =>
-    context.log.info("searches started")
+  def apply(redis: RedisClient): Behavior[StartSearch] = Behaviors.receive { (ctx, _) =>
+    ctx.log.info("searches task creation started")
 
-    implicit val system: ActorSystem[Nothing] = context.system
-    implicit val executionContext: ExecutionContext = context.system.executionContext
-    val publisher = context.spawn(Publisher(redis), "searchTaskPublisher")
+    implicit val system: ActorSystem[Nothing] = ctx.system
+    implicit val executionContext: ExecutionContext = ctx.system.executionContext
+
+    val publisher = ctx.spawn(Publisher(redis), "searchTaskPublisher")
+    val searchRepo = ctx.spawn(SearchRepository(), "searchRepositorySearchExecutor")
 
     Await.result(
-      findActiveSearches
-        .runForeach { case (taskId: String, params: Map[String, String]) =>
-          publisher ! Publish(SEARCH_TASKS_CHANNEL, TaskMessage(taskId, params))
-        }
-        .andThen(_ => publisher ! EndPublish()),
+      tasks(searchRepo)
+        .map { source =>
+          source.runForeach { case (taskId: String, params: Map[String, String]) =>
+            publisher ! Publish(SEARCH_TASKS_CHANNEL, TaskMessage(taskId, params))
+          }.andThen(_ => {
+            publisher ! EndPublish()
+            searchRepo ! EndSearchRepo()
+          })
+        },
       Duration.Inf
     )
 
-    context.log.trace("searches ended")
+    ctx.log.debug("searches task creation ended")
     Behaviors.same
   }
 
-  def findActiveSearches(implicit system: ActorSystem[Nothing]): Source[(String, Map[String, String]), NotUsed] = {
+  def tasks(searchesRepo: ActorRef[SearchRepoMsg])(implicit system: ActorSystem[_]): Future[Source[(String, Map[String, String]), NotUsed]] = {
+    import akka.actor.typed.scaladsl.AskPattern._
+
+    import scala.concurrent.duration._
+
     implicit val context: ExecutionContext = system.executionContext
-    Slick.source(sql"SELECT * FROM search WHERE active = true".as[SearchRow])
-//      .map { row => (row.id, SearchParams(row.brand, row.model, row.minMileage, row.maxMileage)) }
-      .map { _.id }
-      .map { (_, randomUUID.toString, Timestamp.from(Instant.now())) }
-      .via (
-        Slick.flowWithPassThrough { case (searchId: Int, taskId: String, timestamp: Timestamp) =>
-          sqlu"INSERT INTO task (id, search_id, start_time) VALUES($taskId, $searchId, $timestamp)"
-            .map(_ => (searchId, taskId))
-        }
-      )
-      .map { case (searchId: Int, taskId: String) =>
-        val futureParams = Slick.source(sql"SELECT * FROM search_param WHERE search_id = $searchId".as[SearchParamRow])
-          .runWith(Sink.seq[SearchParamRow])
-          .map(seq => seq.map(p => p.name -> p.value).toMap)
-        (taskId, Await.result(futureParams, Duration.Inf))
+    implicit val timeout: Timeout = 15.seconds
+
+
+    val searches: Future[SearchesAnswer] = searchesRepo ? GetActiveSearches
+    searches.map { _.searches }
+      .map { x =>
+        x.map { s => (s.id, s.params, randomUUID.toString, Timestamp.from(Instant.now())) }
+        .via (
+          Slick.flowWithPassThrough { case (searchId: Int, params: Map[String, String], taskId: String, timestamp: Timestamp) =>
+            sqlu"INSERT INTO task (id, search_id, start_time) VALUES($taskId, $searchId, $timestamp)"
+              .map(_ => (taskId, params))
+          }
+        )
       }
+
+
+
+//    val searches: Future[SearchesAnswer] = searchesRepo ? GetActiveSearches
+//    Await.result(searches.map { _.searches }, Duration.Inf)
+//      .map { s => (s.id, s.params, randomUUID.toString, Timestamp.from(Instant.now())) }
+//      .via (
+//        Slick.flowWithPassThrough { case (searchId: Int, params: Map[String, String], taskId: String, timestamp: Timestamp) =>
+//          sqlu"INSERT INTO task (id, search_id, start_time) VALUES($taskId, $searchId, $timestamp)"
+//            .map(_ => (taskId, params))
+//        }
+//      )
   }
 }
--- src/main/scala/eu/jrie/put/cs/pt/scrapper/search/SearchRepository.scala
+++ src/main/scala/eu/jrie/put/cs/pt/scrapper/search/SearchRepository.scala
@@ -8,6 +8,7 @@ import akka.stream.scaladsl.{Sink, Source}
 import eu.jrie.put.cs.pt.scrapper.model.Search
 import eu.jrie.put.cs.pt.scrapper.model.db.Tables.SearchesParamsTable.SearchParamRow
 import eu.jrie.put.cs.pt.scrapper.model.db.Tables.SearchesTable.SearchRow
+import slick.jdbc.SQLActionBuilder
 
 import scala.collection.immutable.ListMap
 import scala.concurrent.duration.Duration
@@ -19,34 +20,49 @@ object SearchRepository {
   import session.profile.api._
 
   sealed trait SearchRepoMsg
-  case class AddSearch(replyTo: ActorRef[SearchRepoMsg]) extends SearchRepoMsg
+  case class AddSearch(replyTo: ActorRef[SearchesAnswer]) extends SearchRepoMsg
   case class GetSearches(userId: Int, active: Option[Boolean], replyTo: ActorRef[SearchesAnswer]) extends SearchRepoMsg
-  case class GetActiveSearches(replyTo: ActorRef[SearchRepoMsg]) extends SearchRepoMsg
+  case class GetActiveSearches(replyTo: ActorRef[SearchesAnswer]) extends SearchRepoMsg
   case class SearchesAnswer(searches: Source[Search, NotUsed]) extends SearchRepoMsg
+  case class EndSearchRepo() extends SearchRepoMsg
 
   def apply(): Behavior[SearchRepoMsg] = Behaviors.receive { (ctx, msg) =>
     implicit val system: ActorSystem[_] = ctx.system
     implicit val executionContext: ExecutionContextExecutor = ctx.executionContext
     msg match {
       case GetSearches(userId, active, replyTo) =>
-        val searches = Slick.source(sql"SELECT * FROM search WHERE user_id = $userId".as[SearchRow])
+        val filtered = Slick.source(sql"SELECT * FROM search WHERE user_id = $userId".as[SearchRow])
           .filter { s => active forall (s.active == _) }
-          .map { searchRow =>
-            val futureParams = Slick.source(sql"SELECT * FROM search_param WHERE search_id = ${searchRow.id}".as[SearchParamRow])
-              .runWith(Sink.seq[SearchParamRow])
-              .map { _.map(p => p.name -> p.value) }
-              .map { _.sortWith(_._1 > _._2) }
-              .map { ListMap.newBuilder.addAll(_).result }
-            (searchRow, Await.result(futureParams, Duration.Inf))
-          }
-          .map { case (searchRow, params) =>
-            Search(searchRow.id, searchRow.userId, params, searchRow.active)
-          }
-        replyTo ! SearchesAnswer(searches)
+        replyTo ! SearchesAnswer(searches(filtered))
+        Behaviors.same
+      case GetActiveSearches(replyTo) =>
+        replyTo ! SearchesAnswer(searches(sql"SELECT * FROM search WHERE active = true"))
+        Behaviors.same
+      case EndSearchRepo() =>
+        Behaviors.stopped
       case _ =>
         ctx.log.info("unsupported repo msg")
+        Behaviors.stopped
     }
+  }
 
-    Behaviors.same
+  private def searches(sql: SQLActionBuilder)(implicit system: ActorSystem[_]): Source[Search, NotUsed] =  {
+    searches(Slick.source(sql.as[SearchRow]))
+  }
+
+  private def searches(source: Source[SearchRow, NotUsed])(implicit system: ActorSystem[_]): Source[Search, NotUsed] = {
+    implicit val executionContext: ExecutionContextExecutor = system.executionContext
+    source
+      .map { searchRow =>
+        val futureParams = Slick.source(sql"SELECT * FROM search_param WHERE search_id = ${searchRow.id}".as[SearchParamRow])
+          .runWith(Sink.seq[SearchParamRow])
+          .map { _.map(p => p.name -> p.value) }
+          .map { _.sortWith(_._1 > _._2) }
+          .map { ListMap.newBuilder.addAll(_).result }
+        (searchRow, Await.result(futureParams, Duration.Inf))
+      }
+      .map { case (searchRow, params) =>
+        Search(searchRow.id, searchRow.userId, params, searchRow.active)
+      }
   }
 }
