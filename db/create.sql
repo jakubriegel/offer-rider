335aa5da44bb645ecc28cbba21a89b55f868a046
jakubriegel <jakubriegel112@gmail.com>
2020-04-13 10:07:17 +0200

    some queries

--- db/create.sql
+++ db/create.sql
@@ -114,17 +114,3 @@ INSERT INTO search_param (search_id, name, value)
 VALUES (@user3search, 'maxMileage', '10');
 INSERT INTO search_param (search_id, name, value)
 VALUES (@user3search, 'minYear', '2018');
-
-# some selects
-SELECT * FROM user;
-SELECT * FROM search;
-SELECT * FROM search_param;
-SELECT * FROM task;
-SELECT * FROM result;
-SELECT * FROM result_param;
-
-SELECT s.id, p.name, p.value
-FROM search as s, search_param as p
-WHERE s.active = true AND s.id = p.search_id;
-
-SELECT * FROM search WHERE user_id = 1 AND active = active;
