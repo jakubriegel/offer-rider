34161ce679ebb1023803f4eaf2a063e0a19f7edc
jakubriegel <jakubriegel112@gmail.com>
2020-04-29 13:53:03 +0200

    docs | png name fix

--- docs/schema/make-schema.sh
+++ docs/schema/make-schema.sh
@@ -1,5 +1,5 @@
 if fc-list | grep -q "JetBrains Mono Medium"; then
-    dot -Tpng -obig-store-schema.png offer-rider.gv
+    dot -Tpng -ooffer-rider-schema.png offer-rider.gv
     exit 0
 else
     echo "To generate the schema get JetBrains Mono typeface from https://www.jetbrains.com/lp/mono/"
