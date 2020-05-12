30b843baa87de3daf27e0dd9451061b5662fca26
jakubriegel <jakubriegel112@gmail.com>
2020-05-12 21:36:32 +0200

    search-service | db deploy fix

--- search-service/db/run-db.sh
+++ search-service/db/run-db.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ -n ${CREATE_DB} ]]; then
-  until mysql -u root -p${MYSQL_ROOT_PASSWORD} < /init.sql; do
+if [ -n "${CREATE_DB}" ]; then
+  until mysql -u root -p"${MYSQL_ROOT_PASSWORD}" < /init.sql; do
     sleep 8
     echo "Waiting for db..."
   done && echo "Provisioning successful" &
